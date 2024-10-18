9c6c695db87bd1b00c706713e766b687de2ff38f
Garciavil <163667289+Garciavil@users.noreply.github.com>
2024-10-18 08:29:11 -0600

    Ejercicio 2 guia sem9

---
+++ ejercicio2uiaS95/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2uiaS95
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventario inventario = new Inventario();
+
+            Console.Write("¿Cuántos productos desea agregar al inventario? ");
+            int cantidadProductos = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cantidadProductos; i++)
+            {
+                Console.Write("Ingrese el nombre del producto: ");
+                string nombre = Console.ReadLine();
+
+                Console.Write("Ingrese el precio del producto: ");
+                decimal precio = decimal.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese la cantidad en stock: ");
+                int cantidad = int.Parse(Console.ReadLine());
+
+                inventario.AgregarProducto(nombre, precio, cantidad);
+            }
+
+            // Ejemplo de actualización y cálculo del valor total
+      
+            decimal valorTotal = inventario.CalcularValorTotal();
+            Console.WriteLine($"El valor total del inventario es: ${valorTotal}");
+
+            // Esperar una tecla antes de cerrar
+            Console.ReadKey();
+        }
+    }
+}
