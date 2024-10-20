8bf4cc7b86e73b1b77c3b696e161cbb40e466a7b
Garciavil <163667289+Garciavil@users.noreply.github.com>
2024-10-19 23:48:19 -0600

    Add files via upload

---
+++ ejercicio4sem9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio4sem9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tamaño del arreglo
+            const int size = 5;
+            int[] numeros = new int[size];
+            int[] factoriales = new int[size];
+
+            // Leer números
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Ingrese el número {i + 1}: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Calcular factoriales usando una clase auxiliar
+            control.Funciones.CalcularFactoriales(numeros, factoriales);
+
+            // Mostrar resultados
+            Console.WriteLine("\nNúmeros originales y sus factoriales:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Número: {numeros[i]}, Factorial: {factoriales[i]}");
+            }
+        }
+    }
+}
